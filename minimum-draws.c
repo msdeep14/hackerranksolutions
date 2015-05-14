d3f102a19a34f6ab2fcdeb50587664da53c9a152
Mandeep Singh <mandeepsinghshekhawat95@gmail.com>
2015-05-15 01:14:05 +0530

    minimum-draws.c

---
+++ minimum-draws.c
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/minimum-draws
+//mandeep singh @msdeep14
+#include<stdio.h>
+int main()
+{
+	int i,j,t,n;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&n);
+		printf("%d\n",n+1);
+	}
+	return 0;
+}

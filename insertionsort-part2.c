d97431c982bdd5420172f3018729c5c62d89657c
Mandeep Singh <mandeepsinghshekhawat95@gmail.com>
2015-05-23 10:41:41 +0530

    insertionsort-part2.c

---
+++ insertionsort-part2.c
@@ -0,0 +1,28 @@
+//insertion sort part2
+#include<stdio.h>
+int main()
+{
+	int i,j,current,s,k;
+	int arr[1010];
+	scanf("%d",&s);
+	for(i=0;i<s;i++)
+		scanf("%d",&arr[i]);
+	for(i = 1; i< s; i++)
+    {
+        current = arr[i];//current value
+        for(j = i-1; j>=0; j--)
+        {
+            if(current < arr[j])
+                arr[j+1] = arr[j];
+            else
+                break;//stop the loop
+        }
+        arr[j+1] = current;//insertion
+		
+		for(k=0;k<s;k++)
+			printf("%d ",arr[k]);
+		printf("\n");
+		
+    }
+	return 0;
+}
